df95ada25eeec65f37b0a41948909adaf4c8e375
nelvisstiphen <nelvissalazar.31@gmail.com>
2022-11-01 21:48:24 -0500

    primer commit

---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+const button = document.querySelector(".button");
+const nav = document.querySelector(".header__nav");
+
+button.addEventListener('click', ()=> {
+    nav.classList.toggle("active")
+})
+
+
+
+
+
+
